8e63dfb0132e3feb4396fb579f31adaf4255342b
MerlinEgalite <egalite.merlin@gmail.com>
2023-05-25 10:22:13 +0200

    feat: add modules setup tests

--- src/interfaces/ISafe.sol
+++ src/interfaces/ISafe.sol
@@ -4,4 +4,6 @@ pragma solidity >=0.5.0;
 import {IAvatar} from "src/interfaces/IAvatar.sol";
 import {Operation} from "src/libraries/Types.sol";
 
-interface ISafe is IAvatar {}
+interface ISafe is IAvatar {
+	function isModuleEnabled(address module) external view returns (bool);
+}
--- test/TestDAOSetup.sol
+++ test/TestDAOSetup.sol
@@ -12,20 +12,42 @@ contract TestDAOSetup is TestSetup {
 		assertEq(Ownable(address(proxyAdmin)).owner(), address(morphoAdmin));
 	}
 
-	function testMorphoDaoRole() public {
+	function testRoleModifier() public {
 		assertEq(Ownable(address(roleModifier)).owner(), address(morphoAdmin));
 		assertEq(IModule(address(roleModifier)).target(), address(morphoAdmin));
 		assertEq(IModule(address(roleModifier)).avatar(), address(morphoAdmin));
 		assertTrue(roleModifier.isModuleEnabled(address(morphoDao)));
-
 	}
 
-	function testDelay() public {
+	function testDelayModifier() public {
 		assertEq(Ownable(address(delayModifier)).owner(), address(morphoAdmin));
 		assertEq(IModule(address(delayModifier)).target(), address(morphoAdmin));
 		assertEq(IModule(address(delayModifier)).avatar(), address(morphoAdmin));
 		assertTrue(delayModifier.isModuleEnabled(address(morphoDao)));
 		assertEq(delayModifier.txCooldown(), 1 days);
 		assertEq(delayModifier.txExpiration(), 0);
+	}
+
+	function testModules() public {
+		// Role 1: operator
+		assertEq(roleModifier.defaultRoles(address(operator)), 1);
+
+		// morphoAdmin should accept tx from delay modifier
+		assertTrue(morphoAdmin.isModuleEnabled(address(delayModifier)));
+
+		// morphoAdmin should accept tx from role modifier
+		assertTrue(morphoAdmin.isModuleEnabled(address(roleModifier)));
+
+		// morphoDao should accept tx from delay modifier
+		assertTrue(morphoAdmin.isModuleEnabled(address(roleModifier)));
+
+		// morphoDao should not accept tx from role modifier
+		assertFalse(morphoDao.isModuleEnabled(address(roleModifier)));
+
+		// delay modifier should accept tx from morphoDao
+		assertTrue(delayModifier.isModuleEnabled(address(morphoDao)));
+
+		// role modifier should accept tx from morphoDao
+		assertTrue(roleModifier.isModuleEnabled(address(morphoDao)));
 	}
 }

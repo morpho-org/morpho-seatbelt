e5cc135626c6af831568fc067eb96677b01ca5a7
Tristan22400 <tristanmartin22400@gmail.com>
2023-06-01 12:12:00 +0200

    fix: add test for reth market

--- test/TestTransactionExecution.sol
+++ test/TestTransactionExecution.sol
@@ -14,6 +14,10 @@ contract TestTransaction is TestSetup {
         assertTrue(market.pauseStatuses.isWithdrawPaused);
         assertTrue(market.pauseStatuses.isLiquidateBorrowPaused);
         assertTrue(market.pauseStatuses.isP2PDisabled);
+        assertFalse(market.pauseStatuses.isSupplyCollateralPaused);
+        assertFalse(market.pauseStatuses.isWithdrawCollateralPaused);
+        assertFalse(market.pauseStatuses.isDeprecated);
+        assertFalse(market.pauseStatuses.isLiquidateCollateralPaused);
         assertEq(market.reserveFactor, 0, "Wrong reserve Factor");
         assertEq(market.p2pIndexCursor, 0, "Wrong p2pIndexCursor");
         assertTrue(market.isCollateral, "Asset not Collateral");

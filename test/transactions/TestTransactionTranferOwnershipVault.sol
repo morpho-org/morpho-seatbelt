d040fc33026b5d1b7b42f00493b34f1d83cf4b54
Tristan22400 <tristanmartin22400@gmail.com>
2023-06-19 17:49:31 +0200

    fix: add test

--- test/transactions/TestTransactionTranferOwnershipVault.sol
+++ test/transactions/TestTransactionTranferOwnershipVault.sol
@@ -3,6 +3,8 @@ pragma solidity >=0.8.0;
 
 import "test/TestTransactionSetup.sol";
 
+import {Ownable2Step} from "@openzeppelin-contracts/contracts/access/Ownable2Step.sol";
+
 contract TestTransactionTransferOwnershipVault is TestTransactionSetup {
     address internal constant ma3WETH = 0x39Dd7790e75C6F663731f7E1FdC0f35007D3879b;
 
@@ -16,6 +18,7 @@ contract TestTransactionTransferOwnershipVault is TestTransactionSetup {
     }
 
     function testAssertionsOfTransaction() public virtual {
-        assertEq(Ownable(address(ma3WETH)).owner(), address(morphoAdmin));
+        assertEq(Ownable2Step(address(ma3WETH)).owner(), address(morphoAdmin));
+        assertEq(Ownable2Step(address(ma3WETH)).pendingOwner(), address(0));
     }
 }

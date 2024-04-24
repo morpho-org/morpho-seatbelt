1f4ba4c3c616434817ed49cf7720aae49c89baa0
Jean-Grimal <83286814+Jean-Grimal@users.noreply.github.com>
2024-04-24 10:45:29 +0200

    fix: tests

--- test/transactions/optimizersPausingTxTest.sol
+++ test/transactions/optimizersPausingTxTest.sol
@@ -8,19 +8,20 @@ contract optimizersPausingTxTest is DelayModifierTxTest {
     address internal constant WBTC = 0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599;
     address internal constant S_DAI = 0x83F20F44975D03b1b09e64809B757c47f942BEeA;
     address internal constant CB_ETH = 0xBe9895146f7AF43049ca1c1AE358B0541Ea49704;
+    address internal constant USDT = 0xdAC17F958D2ee523a2206206994597C13D831ec7;
 
     address[] internal pausedMC2Assets = [mcCOMP, mcWETH, mcUNI, mcUSDT];
-    address[] internal pausedMA3Assets = [DAI, WBTC, S_DAI, mcUSDT, CB_ETH];
+    address[] internal pausedMA3Assets = [DAI, WBTC, S_DAI, USDT, CB_ETH];
 
     function _txName() internal pure override returns (string memory) {
-        return "assetsPausing";
+        return "optimizersPausing";
     }
 
     function _forkBlockNumber() internal virtual override returns (uint256) {
         return 19_723_639;
     }
 
-    function testAssetPausing() public {
+    function testOptimizersPausing() public {
         for (uint256 i; i < pausedMC2Assets.length; i++) {
             address asset = pausedMC2Assets[i];
             IMorphoCompound.MarketPauseStatus memory market = morphoCompound.marketPauseStatus(asset);

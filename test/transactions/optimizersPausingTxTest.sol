e450205d527c5f4d0831a089989808bb81df2984
Rubilmax <rmilon@gmail.com>
2024-04-24 09:27:50 +0200

    test: fix checks & rename tests

--- test/transactions/optimizersPausingTxTest.sol
+++ test/transactions/optimizersPausingTxTest.sol
@@ -1,25 +1,23 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "./helpers/MorphoDaoTxTest.sol";
+import "./helpers/DelayModifierTxTest.sol";
 
-contract assetPausingTxTest is MorphoDaoTxTest {
-    address internal constant ma2CRV = 0x9dc7094530cB1bcf5442c3b9389ee386738A190c;
+contract optimizersPausingTxTest is DelayModifierTxTest {
     address internal constant DAI = 0x6B175474E89094C44Da98b954EedeAC495271d0F;
     address internal constant WBTC = 0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599;
     address internal constant S_DAI = 0x83F20F44975D03b1b09e64809B757c47f942BEeA;
     address internal constant CB_ETH = 0xBe9895146f7AF43049ca1c1AE358B0541Ea49704;
 
     address[] internal pausedMC2Assets = [mcCOMP, mcWETH, mcUNI, mcUSDT];
-    address[] internal pausedMA2Assets = [ma2CRV];
     address[] internal pausedMA3Assets = [DAI, WBTC, S_DAI, mcUSDT, CB_ETH];
 
     function _txName() internal pure override returns (string memory) {
-        return "assetPausing";
+        return "assetsPausing";
     }
 
     function _forkBlockNumber() internal virtual override returns (uint256) {
-        return 19_718_558;
+        return 19_723_639;
     }
 
     function testAssetPausing() public {
@@ -29,14 +27,6 @@ contract assetPausingTxTest is MorphoDaoTxTest {
 
             assertTrue(market.isBorrowPaused, "mc2 isBorrowPaused");
             assertTrue(market.isSupplyPaused, "mc2 isSupplyPaused");
-        }
-
-        for (uint256 i; i < pausedMA2Assets.length; i++) {
-            address asset = pausedMA2Assets[i];
-            IMorphoAaveV2.MarketPauseStatus memory market = morphoAaveV2.marketPauseStatus(asset);
-
-            assertTrue(market.isBorrowPaused, "ma2 isBorrowPaused");
-            assertTrue(market.isSupplyPaused, "ma2 isSupplyPaused");
         }
 
         for (uint256 i; i < pausedMA3Assets.length; i++) {
